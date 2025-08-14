784a1bb518e7cdd2b870af3b3335527e830fd50d
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-08-15 05:48:14 +0600

    Topbar designed

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,37 @@
-'use client'
+// 'use client'
 
-import { useAuthProvider } from "@/components/context/AuthContext";
+// import { useAuthProvider } from "@/components/context/AuthContext";
 
-export default function Home() {
-const {loading,user} =useAuthProvider()
-console.log(user);
+// export default function Home() {
+// const {loading,user} =useAuthProvider()
+// console.log(user);
 
-if (loading) {
-  return 'koa'
-}
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      {user?.name}
-    </div>
-  );
-}
+// if (loading) {
+//   return 'koa'
+// }
+//   return (
+//     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+//       {user?.name}
+//     </div>
+//   );
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

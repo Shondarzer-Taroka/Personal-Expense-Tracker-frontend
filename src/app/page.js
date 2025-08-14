baf5023d3326fdfa25c6ce923b5858f0bc026776
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-08-15 05:55:13 +0600

    layout designed

--- src/app/page.js
+++ src/app/page.js
@@ -35,3 +35,43 @@
 
 
 
+'use client'
+import { useAuthProvider } from "@/components/context/AuthContext";
+import DashboardCards from "@/components/Dashboard/DashboardCards";
+import { redirect, useRouter } from "next/navigation";
+
+export default function DashboardPage() {
+  const { user, loading } = useAuthProvider()
+  const router = useRouter()
+
+
+  if (loading) {
+    return <div className="flex items-center justify-center min-h-screen">
+      <div className="flex flex-col items-center">
+        <div className="w-16 h-16 border-4 border-red-600 border-t-transparent rounded-full animate-spin"></div>
+        <p className="mt-4 text-lg font-medium text-gray-700">লোড হচ্ছে...</p>
+      </div>
+    </div>
+  }
+
+
+  if (!user?.email) {
+    return redirect('/login'); 
+  }
+
+  if (user.role !== 'admin') {
+    router.push('/news/dashboard/myProfile')
+  }
+
+  return (
+
+    user.role === 'admin' && <div>
+      <h2 className="text-2xl font-bold mb-4">ড্যাশবোর্ড ওভারভিউ</h2>
+      <DashboardCards />
+    </div>
+
+  );
+}
+
+
+
